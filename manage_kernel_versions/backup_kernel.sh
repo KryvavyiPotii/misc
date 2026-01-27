0854e1c4168cc01914f413124e793678d40f5f02
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2026-01-27 10:36:27 +0100

    Update backup_kernel.sh

--- manage_kernel_versions/backup_kernel.sh
+++ manage_kernel_versions/backup_kernel.sh
@@ -4,26 +4,30 @@
 . /usr/local/bin/libutil.sh
 
 
-readonly USAGE="Usage $(basename $0) KERNELVERSION"
+readonly ERR_PRIV=4
+readonly ERR_INVALID_INPUT=5
+
+readonly USAGE="Usage: $(basename $0) KERNELVERSION"
+
+
+if ! is_root; then
+    echo "Should be executed as root"
+    exit $ERR_PRIV
+fi
 
 readonly kernel_version="$1"
-readonly uid="$(id -u)"
 
-
-if ! is_root "$uid"; then
-    echo "Should be executed as root"
-    exit 1
-fi
-
-if ! is_valid_kernel_version "$kernel_version"; then 
+if ! is_valid_kernel_version "$kernel_version"; then
     echo "$USAGE"
-    exit 1
+    exit $ERR_INVALID_INPUT
 fi
-
 
 readonly old_umask=$(umask)
 readonly tmp_umask=0377
 
 umask "$tmp_umask"
-tar -zcf "/kernel_bp/${kernel_version}.tar.gz" -P /boot/*"$kernel_version"*
+tar -Pzcf "/kernel_bp/${kernel_version}.tar.gz" \
+    /boot/*"$kernel_version"* \
+    /usr/src/*"$kernel_version"* \
+    /lib/modules/*"$kernel_version"*
 umask "$old_umask"
