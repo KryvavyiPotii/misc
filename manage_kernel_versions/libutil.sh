9dc2bb585da311f89b03573348751c80c6bed205
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2026-01-27 10:35:02 +0100

    Update libutil.sh

--- manage_kernel_versions/libutil.sh
+++ manage_kernel_versions/libutil.sh
@@ -1,9 +1,10 @@
 readonly ROOT_UID=0
-readonly VERSION_PATTERN='^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(_[[:digit:]]+|())$'
+readonly KERN_VERSION_PATTERN='^[0-9]+\.[0-9]+\.[0-9]+(_[0-9]+|())$'
+readonly HARD_DRIVE_PATTERN='^/dev/sd[a-z]$'
 
 
 is_root() {
-    readonly _uid="$1"
+    readonly _uid="$(id -u)"
 
     [ "$_uid" = "$ROOT_UID" ]
 }
@@ -11,5 +12,11 @@ is_root() {
 is_valid_kernel_version() {
     readonly _kernel_version="$1"
 
-    echo "$_kernel_version" | grep -E -q "$VERSION_PATTERN"
+    echo "$_kernel_version" | grep -E -q "$KERN_VERSION_PATTERN"
+}
+
+is_valid_hard_drive() {
+    readonly _hard_drive="$1"
+
+    echo "$_hard_drive" | grep -E -q "$HARD_DRIVE_PATTERN"
 }
