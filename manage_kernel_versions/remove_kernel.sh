d4c29ea935afb7cc0dac1b8b68b3fb8e17331128
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2026-01-27 10:35:40 +0100

    Update remove_kernel.sh

--- manage_kernel_versions/remove_kernel.sh
+++ manage_kernel_versions/remove_kernel.sh
@@ -4,23 +4,26 @@
 . /usr/local/bin/libutil.sh
 
 
-readonly USAGE="Usage $(basename $0) KERNELVERSION"
+readonly ERR_PRIV=4
+readonly ERR_INVALID_INPUT=5
+
+readonly USAGE="Usage: $(basename $0) KERNELVERSION"
+
+
+if ! is_root; then
+    echo "Should be executed as root"
+    exit $ERR_PRIV
+fi
 
 readonly kernel_version="$1"
-readonly uid="$(id -u)"
 
-
-if ! is_root "$uid"; then
-    echo "Should be executed as root"
-    exit 1
-fi
-
-if ! is_valid_kernel_version "$kernel_version"; then 
+if ! is_valid_kernel_version "$kernel_version"; then
     echo "$USAGE"
-    exit 1
+    exit $ERR_INVALID_INPUT
 fi
 
 
-rm /boot/*"$kernel_version"* >/dev/null
-rm -r /usr/src/*"$kernel_version"* >/dev/null
-rm -r /lib/modules/*"$kernel_version"* >/dev/null
+rm -r \
+    /boot/*"$kernel_version"* \
+    /usr/src/*"$kernel_version"* \
+    /lib/modules/*"$kernel_version"* >/dev/null#!/bin/sh
